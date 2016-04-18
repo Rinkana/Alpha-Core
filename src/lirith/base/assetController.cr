f86b2a4e66e79de076ca3f17465d2b170edf58a4
Max Berends <berendsmax@gmail.com>
2016-04-18 20:01:09 +0200

    Added mimetype dep

--- src/lirith/base/assetController.cr
+++ src/lirith/base/assetController.cr
@@ -2,13 +2,11 @@ module Lirith
   class AssetController < BaseController
 
     def get(request, response)
-      response.content_type = "application/octet-stream"
-      response.content_type = "image/png"
+      #response.content_type = "application/octet-stream"
+      #response.content_type = "image/png"
+      #TODO: detect mime type
+      response.content_type = "application/unknown"
       response.view = File.read("/vagrant/App/src/App/public/image/favicon.png")
-      #File.open("/vagrant/App/src/App/public/image/favicon.png") do |file|
-      #  IO.copy(file, response.view)
-      #end
-      #puts file
     end
 
   end
